aba8fe57f3ae992bf3553c5a00999e7fd5a2a572
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2022-12-01 23:00:00 +0800

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var currentElf = 0
+        var maxElf = -1
+
+        input.forEach {
+            if (it.isEmpty()) {
+                maxElf = maxOf(maxElf, currentElf)
+                currentElf = 0
+            } else {
+                currentElf += it.toInt()
+            }
+        }
+
+        return maxElf
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfRations = mutableListOf<Int>()
+        var currentElf = 0
+
+        input.forEach {
+            if (it.isEmpty()) {
+                elfRations.add(currentElf)
+                currentElf = 0
+            } else {
+                currentElf += it.toInt()
+            }
+        }
+
+        return elfRations.sorted().reversed().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
