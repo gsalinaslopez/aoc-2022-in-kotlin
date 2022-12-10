36ce8fb17593620b1f5cabc9b0643e3e050b200b
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2022-12-10 23:19:00 +0800

    Day 10 Solution | Part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,32 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun displayCRT(input: List<String>): Int {
         var x = 1
         var acc = 0
         var i = 1
         input.forEach { instruction ->
-            println("beginning of $i cycle: ${x}")
+            if ((i - 1) % 40 == 0) println()
+            if ((i - 1) % 40 in x - 1..x + 1) print("#") else print(".")
 
-            if ((i - 20) % 40 == 0) {
-                println(x * i)
-                acc += (x * i)
-            }
-
-            println("end of $i cycle: ${x}")
-
+            if ((i - 20) % 40 == 0) acc += (x * i)
             if (instruction.contains("addx")) {
                 i++
-                println("beginning of $i cycle: ${x}")
 
-                if ((i - 20) % 40 == 0) {
-                    println(x * i)
-                    acc += (x * i)
-                }
+                if ((i - 1) % 40 == 0) println()
+                if ((i - 1) % 40 in x - 1..x + 1) print("#") else print(".")
+
+                if ((i - 20) % 40 == 0) acc += (x * i)
 
                 x += instruction.split(" ")[1].toInt()
-
-                println("end of $i cycle: ${x}")
-            } else {
-                0
             }
             i++
         }
@@ -34,19 +24,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    var testInput = readInput("Day07_test")
-    //part1(testInput)
-    testInput = readInput("Day07_test2")
-    println(part1(testInput))
-    check(part1(testInput) == 13140)
+    val testInput = readInput("Day07_test")
+    check(displayCRT(testInput) == 13140)
 
     val input = readInput("Day07")
-    println(part1(input))
-    /*
-    check(part1(buildTreeGrid(testInput)) == 21)
-    check(part2(buildTreeGrid(testInput)) == 8)
-
-    println(part2(buildTreeGrid(input)))
-    */
+    displayCRT(input)
 }
 
